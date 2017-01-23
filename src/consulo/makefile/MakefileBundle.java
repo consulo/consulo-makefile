d57619c12250be8c52fc6fddfb7ae821b17c1a51
VISTALL <vistall.valeriy@gmail.com>
2017-01-23 18:31:52 +0300

    compilation

--- src/consulo/makefile/MakefileBundle.java
+++ src/consulo/makefile/MakefileBundle.java
@@ -1,12 +1,28 @@
 package consulo.makefile;
 
-import consulo.lombok.annotations.Bundle;
+import org.jetbrains.annotations.PropertyKey;
+import com.intellij.AbstractBundle;
 
 /**
  * @author VISTALL
  * @since 18:40/16.03.13
  */
-@Bundle
-public class MakefileBundle
+public class MakefileBundle extends AbstractBundle
 {
+	private static final MakefileBundle ourInstance = new MakefileBundle();
+
+	private MakefileBundle()
+	{
+		super("messages.MakefileBundle");
+	}
+
+	public static String message(@PropertyKey(resourceBundle = "messages.MakefileBundle") String key)
+	{
+		return ourInstance.getMessage(key);
+	}
+
+	public static String message(@PropertyKey(resourceBundle = "messages.MakefileBundle") String key, Object... params)
+	{
+		return ourInstance.getMessage(key, params);
+	}
 }
